e707cbaa991ee31f06acb59e3b7865ae797e85e5
Tharindu Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2024-10-24 15:56:57 +0530

    feat(interface, service): add GetPaymentSourcesAsync method for retrieving available payment sources

--- src/QuickAccounting/QuickAccounting/Repository/Interface/IAccountLedger.cs
+++ src/QuickAccounting/QuickAccounting/Repository/Interface/IAccountLedger.cs
@@ -6,5 +6,13 @@ namespace QuickAccounting.Repository.Interface
     public interface IAccountLedger
     {
         Task<List<AccountLedger>> GetSalesLedgers();
+
+        /// <summary>
+        /// Asynchronously retrieves a list of available payment sources.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task"/> that represents the asynchronous operation, containing a list of <see cref="AccountLedger"/> objects representing the payment sources.
+        /// </returns>
+        Task<List<AccountLedgerView>> GetPaymentSourcesAsync();
     }
 }
--- src/QuickAccounting/QuickAccounting/Repository/Repository/AccountLedgerService.cs
+++ src/QuickAccounting/QuickAccounting/Repository/Repository/AccountLedgerService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuickAccounting.Data;
 using QuickAccounting.Data.Inventory;
+using QuickAccounting.Data.Setting;
 using QuickAccounting.Repository.Interface;
 
 namespace QuickAccounting.Repository.Repository
 {
-    public class AccountLedgerService: IAccountLedger
+    public class AccountLedgerService : IAccountLedger
     {
         private readonly ApplicationDbContext _context;
         public AccountLedgerService(ApplicationDbContext context)
@@ -14,8 +15,31 @@ namespace QuickAccounting.Repository.Repository
         }
         public async Task<List<AccountLedger>> GetSalesLedgers()
         {
-            return await _context.AccountLedger.Where(x=>x.AccountGroupId == 10).ToListAsync();    
+            return await _context.AccountLedger.Where(x => x.AccountGroupId == 10).ToListAsync();
         }
 
+        // Retrieves a list of available payment sources.
+        public async Task<List<AccountLedgerView>> GetPaymentSourcesAsync()
+        {
+            try
+            {
+                var result = await (from al in _context.AccountLedger
+                                    join ag in _context.AccountGroup on al.AccountGroupId equals ag.AccountGroupId
+                                    where new[] { 27, 28 }.Contains(al.AccountGroupId)
+                                    select new AccountLedgerView
+                                    {
+                                        LedgerId = al.LedgerId,
+                                        LedgerName = al.LedgerName,
+                                        LedgerCode = al.LedgerCode,
+                                    }).ToListAsync();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving payment sources: {ex.Message}");
+                throw new Exception("An error occurred while fetching payment sources.", ex);
+            }
+        }
     }
 }
