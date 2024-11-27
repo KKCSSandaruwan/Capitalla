bdb9970c3b8d6a8b5888d38d61f83feafc551edb
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-11-27 15:25:24 +0530

    feat: create NavigationMenu entity model with foreign keys and validation for navigation menu settings

---
+++ src/QuickAccounting/QuickAccounting/Data/Setting/Navigation/NavigationMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuickAccounting.Data.Setting.Navigation
+{
+    public class NavigationMenu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NavigationMenuId { get; set; }
+
+        [Required(ErrorMessage = "Menu group ID is required.")]
+        [ForeignKey("MenuGroup")]
+        public int MenuGroupId { get; set; }
+
+        [Required(ErrorMessage = "Main menu ID is required.")]
+        [ForeignKey("MainMenu")]
+        public int MainMenuId { get; set; }
+
+        [Required(ErrorMessage = "Sub menu ID is required.")]
+        [ForeignKey("SubMenu")]
+        public int SubMenuId { get; set; }
+
+        [Required(ErrorMessage = "Created by is required.")]
+        [StringLength(50, ErrorMessage = "Created by cannot exceed 50 characters.")]
+        public string CreatedBy { get; set; }
+
+        [Required(ErrorMessage = "Creation date is required.")]
+        public DateTime CreatedDate { get; set; }
+
+        [StringLength(50, ErrorMessage = "Modified by cannot exceed 50 characters.")]
+        public string? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [Required(ErrorMessage = "Active status is required.")]
+        public bool Active { get; set; } = true;
+    }
+}
