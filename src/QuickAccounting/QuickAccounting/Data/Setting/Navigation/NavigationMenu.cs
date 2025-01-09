7f52fe6018f8f7e2cb0379243ebf5aa5b0f0cac6
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2025-01-09 17:20:07 +0530

    feat: create Navigation Menu entity modal with validation attributes

--- src/QuickAccounting/QuickAccounting/Data/Setting/Navigation/NavigationMenu.cs
+++ src/QuickAccounting/QuickAccounting/Data/Setting/Navigation/NavigationMenu.cs
@@ -9,17 +9,16 @@ namespace QuickAccounting.Data.Setting.Navigation
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int NavigationMenuId { get; set; }
 
-        [Required(ErrorMessage = "Menu group ID is required.")]
+        [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "Menu group is required.")]
         [ForeignKey("MenuGroup")]
         public int MenuGroupId { get; set; }
 
-        [Required(ErrorMessage = "Main menu ID is required.")]
+        [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "Main menu is required.")]
         [ForeignKey("MainMenu")]
         public int MainMenuId { get; set; }
 
-        [Required(ErrorMessage = "Sub menu ID is required.")]
         [ForeignKey("SubMenu")]
-        public int SubMenuId { get; set; }
+        public int? SubMenuId { get; set; }
 
         [Required(ErrorMessage = "Created by is required.")]
         [StringLength(50, ErrorMessage = "Created by cannot exceed 50 characters.")]
@@ -35,5 +34,13 @@ namespace QuickAccounting.Data.Setting.Navigation
 
         [Required(ErrorMessage = "Active status is required.")]
         public bool Active { get; set; } = true;
+
+
+        // Navigation properties
+        public virtual MenuGroup? MenuGroup { get; set; }
+
+        public virtual MainMenu? MainMenu { get; set; }
+
+        public virtual SubMenu? SubMenu { get; set; }
     }
 }
