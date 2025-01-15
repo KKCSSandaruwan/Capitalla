2a63096bcabe53e92a712fec14384e98474c3651
KKCSSandaruwan <88526475+KKCSSandaruwan@users.noreply.github.com>
2025-01-15 09:07:48 +0530

    Update ReceiptDetailsDup.cs

--- src/QuickAccounting/QuickAccounting/Data/Inventory/ReceiptDetailsDup.cs
+++ src/QuickAccounting/QuickAccounting/Data/Inventory/ReceiptDetailsDup.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QuickAccounting.Data.Inventory
@@ -7,32 +7,46 @@ namespace QuickAccounting.Data.Inventory
     public class ReceiptDetailsDup
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ReceiptDetailsId { get; set; }
 
+        [Required]
+        [ForeignKey("ReceiptMasterDup")]
         public int ReceiptMasterId { get; set; }
 
+        [Required]
         public int SalesMasterId { get; set; }
 
-        public string SerialNo { get; set; }
+        [StringLength(255)]
+        public string? SerialNo { get; set; }
 
+        [StringLength(255)]
         public string VoucherNo { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string TransactionStatus { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal ReceiveableAmount { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal ReceivedAmount { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal DueAmount { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string PaymentStatus { get; set; }
 
         public string AddedBy { get; set; }
-
-        public DateTime AddedDate { get; set; }
+        public DateTime? AddedDate { get; set; }
 
         public string? ModifiedBy { get; set; }
-
         public DateTime? ModifiedDate { get; set; }
     }
 }
